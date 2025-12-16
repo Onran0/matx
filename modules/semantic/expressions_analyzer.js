1ebf019c6b34f91e59b490b8f9b74528313249d8
Onran <kotshel2009@gmail.com>
2025-12-16 10:58:01 +0900

    created expressions analyzer

--- modules/semantic/expressions_analyzer.js
+++ modules/semantic/expressions_analyzer.js
@@ -19,16 +19,25 @@
  ***/
 
 import * as expressions from "../constructions/expressions.js"
+import {BinaryTable,UnaryTable,ConstructorsTable,IndexableTypesTable} from "./types_meta.js";
 import {Token} from "../parse/lexer.js"
+import {getEntityProperty} from "./analyzer.js";
 
 class ExpressionAnalyzer {
     #expressionType
+    #resultProvider
 
-    constructor(expressionType) {
+    constructor(expressionType, resultProvider) {
         this.#expressionType = expressionType
+        this.resultProvider = resultProvider
     }
 
-    analyze(expression, context, pushError) {}
+    // must return type of output expression result, or undefined/null if expression have error
+    analyze(expression, context, pushError) {
+        return typeof this.#resultProvider == "function" ?
+            this.#resultProvider(expression, context, pushError) :
+            this.#resultProvider
+    }
 
     reset() {}
 
@@ -43,7 +52,22 @@ class BinaryExpressionAnalyzer extends ExpressionAnalyzer {
     }
 
     analyze(expression, context, pushError) {
+        if(!Token.isOperator(expression.operation)) {
+            pushError(expression, `undefined operator '${expression.operation}'`)
+        } else {
+            const leftType = analyzeExpression(expression.leftExpression, context, pushError)
+            const rightType = analyzeExpression(expression.leftExpression, context, pushError)
 
+            if(leftType != null && rightType != null) {
+                const outputType = BinaryTable[leftType][expression.operation][rightType]
+
+                if(outputType == null) {
+                    pushError(expression, `operation '${leftType} ${expression.operation} ${rightType}' is undefined`)
+                }
+
+                return outputType
+            }
+        }
     }
 }
 
@@ -53,7 +77,13 @@ class VariableExpressionAnalyzer extends ExpressionAnalyzer {
     }
 
     analyze(expression, context, pushError) {
+        const type = getEntityProperty(context, "var", expression.name, "type")
 
+        if(type == null) {
+            pushError(expression, `variable '${expression.name}' is not defined`)
+        }
+
+        return type
     }
 }
 
@@ -63,7 +93,38 @@ class NewObjectExpressionAnalyzer extends ExpressionAnalyzer {
     }
 
     analyze(expression, context, pushError) {
+        if(Token.isType(expression.type)) {
+            if(ConstructorsTable[expression.type]) {
+                let constructorTypes = [ ]
 
+                for(const argumentExpression of expression.elements) {
+                    const type = analyzeExpression(context, argumentExpression, pushError)
+
+                    if(type == null)
+                        return
+
+                    constructorTypes.push(type)
+                }
+
+                if(ConstructorsTable[expression.type].find(x => {
+                    let match = true
+
+                    x.every((cType, index) => {
+                        if(cType !== constructorTypes[index]) {
+                            match = false
+                            return false
+                        }
+                        return true
+                    })
+
+                    return match
+                }) == null) {
+                    pushError(expression, `constructor '${expression.type}(${constructorTypes.join(", ")})' is not defined`)
+                }
+
+                return expression.type
+            } else pushError(expression, `type '${expression.type}' has no constructors'`)
+        } else pushError(expression, `unknown type '${expression.type}'`)
     }
 }
 
@@ -73,7 +134,40 @@ class FunctionExpressionAnalyzer extends ExpressionAnalyzer {
     }
 
     analyze(expression, context, pushError) {
+        const type = getEntityProperty(context, "function", expression.name, "type")
 
+        if(type == null) {
+            pushError(expression, `function '${expression.name}' is not defined`)
+        } else {
+            let types = [ ]
+
+            for(const argumentExpression of expression.argumentsExpressions) {
+                const type = analyzeExpression(context, argumentExpression, pushError)
+
+                if(type == null)
+                    return
+
+                types.push(type)
+            }
+
+            let match = true
+
+            getEntityProperty(context, "function", expression.name, "argsTypes").every(
+                (argType, index) => {
+                    if(argType !== types[index]) {
+                        match = false
+                        return false
+                    }
+                    return true
+                }
+            )
+
+            if(!match) {
+                pushError(expression, `function '${expression.name}' does not accept arguments '${types.join(",")}'`)
+            }
+        }
+
+        return type
     }
 }
 
@@ -83,7 +177,23 @@ class IndexExpressionAnalyzer extends ExpressionAnalyzer {
     }
 
     analyze(expression, context, pushError) {
+        const type = analyzeExpression(context, expression, pushError)
 
+        if(type != null) {
+            if(type !== Token.TYPE_INT) {
+                pushError(expression, `index must be int`)
+            }
+
+            const targetType = analyzeExpression(context, expression.target, pushError)
+
+            if(targetType != null) {
+                const outputType = IndexableTypesTable[targetType]
+
+                if(outputType == null) {
+                    pushError(expression, `type '${targetType}' is not indexable`)
+                } else return outputType
+            }
+        }
     }
 }
 
@@ -93,7 +203,15 @@ class UnaryExpressionAnalyzer extends ExpressionAnalyzer {
     }
 
     analyze(expression, context, pushError) {
+        const type = analyzeExpression(context, expression.operandExpression, pushError)
 
+        if(type != null) {
+            if(!UnaryTable[type].includes(expression.operatorType)) {
+                pushError(expression, `unary operator '${expression.operatorType}' is not defined for type '${type}'`)
+            }
+
+            return type
+        }
     }
 }
 
@@ -101,6 +219,14 @@ function getAnalyzers() {
     return Object.freeze({
         "bin_analyzer": new BinaryExpressionAnalyzer(),
         "var_analyzer": new VariableExpressionAnalyzer(),
+        "num_analyzer": new ExpressionAnalyzer(
+            expressions.NumberExpression,
+            exp => exp.isInteger() ? Token.TYPE_INT : Token.TYPE_FLOAT
+        ),
+        "bool_analyzer": new ExpressionAnalyzer(
+            expressions.BoolExpression,
+            Token.TYPE_BOOL
+        ),
         "new_object_analyzer": new NewObjectExpressionAnalyzer(),
         "function_analyzer": new FunctionExpressionAnalyzer(),
         "index_analyzer": new IndexExpressionAnalyzer(),
@@ -112,6 +238,6 @@ export function analyzeExpression(context, expression, pushError) {
     const analyzer = getAnalyzers().find(x => x.canAnalyze(expression))
 
     if(analyzer != null) {
-        analyzer.analyze(expression, context, pushError)
-    } else pushError("can't analyze expression of this type")
+        return analyzer.analyze(expression, context, pushError)
+    } else pushError(expression, "can't analyze expression of this type")
 }
